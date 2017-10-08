45749a70475f38edf1d2471f01ad7d0f514f847d
Mikael Valot <mikael.valot@gmail.com>
2017-10-08 20:41:53 +0100

    Factorized unit tests, Vector implementation

--- src/main/scala/sparklite/DatasetAble.scala
+++ src/main/scala/sparklite/DatasetAble.scala
@@ -2,15 +2,19 @@ package sparklite
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{Dataset, Encoder, KeyValueGroupedDataset}
-import sparklite.KeyValueGroupedDatasetAble.RDDGroup
+import sparklite.KeyValueGroupedDatasetAble.{RDDGroup, VectorGroup}
 
 import scala.reflect.ClassTag
+import scala.collection.breakOut
 
+// TODO implement syntax implicit class
 trait DatasetAble[F[_], G[_, _]] {
 
   def map[T, U: ClassTag : Encoder](ft: F[T])(f: T => U): F[U]
 
-  def groupByKey[T, K: Encoder: ClassTag](ft: F[T])(func: T => K): G[K, T] = ???
+  def groupByKey[T, K: Encoder: ClassTag](ft: F[T])(func: T => K): G[K, T]
+
+  def collect[T: ClassTag](ft: F[T]): Array[T]
 
 }
 
@@ -21,12 +25,21 @@ trait KeyValueGroupedDatasetAble[F[_], G[_, _]] {
 
 object KeyValueGroupedDatasetAble {
   type RDDGroup[K, V] = RDD[(K, Iterable[V])]
+  type VectorGroup[K, V] = Map[K, Vector[V]]
+
+  implicit val vectorImpl: KeyValueGroupedDatasetAble[Vector, VectorGroup] =
+    new KeyValueGroupedDatasetAble[Vector, VectorGroup] {
+      def mapGroups[K, V, U: Encoder : ClassTag](kvGrouped: VectorGroup[K, V])(f: (K, Iterator[V]) => U) =
+        kvGrouped.map { case (k, vs) => f(k, vs.iterator) }(breakOut)
+    }
+
   implicit val datasetImpl: KeyValueGroupedDatasetAble[Dataset, KeyValueGroupedDataset] =
     new KeyValueGroupedDatasetAble[Dataset, KeyValueGroupedDataset] {
       def mapGroups[K, V, U: Encoder: ClassTag](kvGrouped: KeyValueGroupedDataset[K, V])
                                      (f: (K, Iterator[V]) => U)
       : Dataset[U] =
         kvGrouped mapGroups f
+
     }
 
   implicit val rddImpl: KeyValueGroupedDatasetAble[RDD, RDDGroup] =
@@ -39,23 +52,32 @@ object KeyValueGroupedDatasetAble {
 }
 
 object DatasetAble {
-  implicit val vectorImpl: DatasetAble[Vector, Map] = new DatasetAble[Vector, Map] {
+  implicit val vectorImpl: DatasetAble[Vector, VectorGroup] = new DatasetAble[Vector, VectorGroup] {
     def map[T, U: ClassTag : Encoder](t: Vector[T])(f: (T) => U) = t map f
+
+    override def groupByKey[T, K: Encoder : ClassTag](ft: Vector[T])(func: (T) => K) =
+      ft groupBy func
+
+    def collect[T: ClassTag](ft: Vector[T]) = ft.toArray
   }
 
   implicit val rddImpl: DatasetAble[RDD, RDDGroup] = new DatasetAble[RDD, RDDGroup] {
     def map[T, U: ClassTag : Encoder](t: RDD[T])(f: (T) => U): RDD[U] = t map f
 
-    override def groupByKey[T, K: Encoder: ClassTag](ft: RDD[T])(func: (T) => K) =
+    def groupByKey[T, K: Encoder: ClassTag](ft: RDD[T])(func: (T) => K) =
       ft groupBy func
+
+    def collect[T: ClassTag](ft: RDD[T]) = ft.collect()
   }
 
   implicit val datasetImpl: DatasetAble[Dataset, KeyValueGroupedDataset] = new DatasetAble[Dataset, KeyValueGroupedDataset] {
 
     def map[T, U: ClassTag : Encoder](t: Dataset[T])(f: (T) => U): Dataset[U] = t map f
 
-    override def groupByKey[T, K: Encoder: ClassTag](ft: Dataset[T])(func: (T) => K): org.apache.spark.sql.KeyValueGroupedDataset[K, T] =
+    def groupByKey[T, K: Encoder: ClassTag](ft: Dataset[T])(func: (T) => K): org.apache.spark.sql.KeyValueGroupedDataset[K, T] =
       ft groupByKey func
+
+    def collect[T: ClassTag](ft: Dataset[T]) = ft.collect()
   }
 }
 
--- src/test/scala/sparklite/DatasetAbleSpecs.scala
+++ src/test/scala/sparklite/DatasetAbleSpecs.scala
@@ -1,55 +1,54 @@
 package sparklite
 
-import org.apache.spark.sql.{Dataset, KeyValueGroupedDataset, SparkSession}
-import org.apache.spark.{SparkConf, SparkContext}
 import DatasetAbleSpecs._
 import TestCommon._
-import sparklite.DatasetAble.datasetImpl
+import sparklite.DatasetAble._
 
 class DatasetAbleSpecs extends org.specs2.mutable.Specification {
 
-  // TODO factorize unit tests
-  // TODO implement for Vector
   val data = Vector(Person1, Person2, Person3)
+
   import session.implicits._
 
   "the DatasetAble implementation for Spark Dataset" should {
     val ds = session.createDataset(data)
-    val dsAble = DatasetAble.datasetImpl
-    val groupAble = KeyValueGroupedDatasetAble.datasetImpl
-    "map" in {
-      val actual: Dataset[Int] = dsAble.map(ds)(_.age)
-      actual.collect().toVector must_=== data.map(_.age)
-    }
-
-    "groupByKey then mapGroups" in {
-      val group = dsAble.groupByKey(ds)(_.age)
-      val actual: Dataset[(Int, Seq[String])] =
-        groupAble.mapGroups(group)((k, vs) => (k, vs.map(_.name).toVector))
-      actual.collect().toSet must_=== Set((25, Vector("bob")), (45, Vector("roger", "john")))
-    }
+    test(ds)
   }
 
   "the DatasetAble implementation for Spark RDD" should {
     val ds = session.sparkContext.makeRDD(data)
-    val dsAble = DatasetAble.rddImpl
-    val groupAble = KeyValueGroupedDatasetAble.rddImpl
+    test(ds)
+  }
+
+  "the DatasetAble implementation for Vector" should {
+    val ds = data
+    test(ds)
+  }
+
+
+
+
+  def test[F[_], G[_, _]](ds: F[Person])(implicit dsAble: DatasetAble[F, G],
+                                         groupAble: KeyValueGroupedDatasetAble[F, G]) = {
     "map" in {
-      val actual = dsAble.map(ds)(_.age)
-      actual.collect().toVector must_=== data.map(_.age)
+      val actual: F[Int] = dsAble.map(ds)(_.age)
+      dsAble.collect(actual).toVector must_=== data.map(_.age)
     }
 
     "groupByKey then mapGroups" in {
       val group = dsAble.groupByKey(ds)(_.age)
-      val actual = groupAble.mapGroups(group)((k, vs) => (k, vs.map(_.name).toVector))
-      actual.collect().toSet must_=== Set((25, Vector("bob")), (45, Vector("roger", "john")))
+      val actual: F[(Int, Seq[String])] =
+        groupAble.mapGroups(group)((k, vs) => (k, vs.map(_.name).toVector))
+      dsAble.collect(actual).toSet must_=== Set((25, Vector("bob")), (45, Vector("roger", "john")))
     }
   }
 
 }
 
 object DatasetAbleSpecs {
+
   case class Person(name: String, age: Int)
+
   val Person1 = Person("bob", 25)
   val Person2 = Person("roger", 45)
   val Person3 = Person("john", 45)
